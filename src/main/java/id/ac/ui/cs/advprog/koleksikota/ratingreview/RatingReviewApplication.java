27d8d5de5ffe9c53bdafbf8c049a9bdfe19a068b
rzapriono <apriono.reza@gmail.com>
2024-04-17 10:03:50 +0700

    Initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/koleksikota/ratingreview/RatingReviewApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.koleksikota.ratingreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatingReviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/koleksikota/ratingreview/RatingReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.koleksikota.ratingreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
