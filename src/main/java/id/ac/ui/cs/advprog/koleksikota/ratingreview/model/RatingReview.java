9d44568eae0f54c9e8fb10b507380681c983c0ea
rzapriono <apriono.reza@gmail.com>
2024-05-01 18:04:58 +0700

    [GREEN] Implement RatingReview model and ApprovalStatus enum

---
+++ src/main/java/id/ac/ui/cs/advprog/koleksikota/ratingreview/model/RatingReview.java
@@ -0,0 +1,32 @@
+package id.ac.ui.cs.advprog.koleksikota.ratingreview.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+import id.ac.ui.cs.advprog.koleksikota.ratingreview.model.Box;
+import id.ac.ui.cs.advprog.koleksikota.ratingreview.enums.ApprovalStatus;
+
+@Getter @Setter
+public class RatingReview {
+    private String ratingReviewId;
+    private String reviewer;
+    private int rating;
+    private String review;
+    private String approvalStatus;
+
+    public RatingReview(String reviewer, String review, int rating) {
+        this.ratingReviewId = UUID.randomUUID().toString();
+        this.reviewer = reviewer;
+        this.review = review;
+        setRating(rating);
+        this.approvalStatus = ApprovalStatus.PENDING.getValue();
+    }
+
+    public void setRating(int rating) {
+        if (rating < 1 || rating > 5) {
+            throw new IllegalArgumentException("Rating tidak valid!");
+        }
+        this.rating = rating;
+    }
+}
