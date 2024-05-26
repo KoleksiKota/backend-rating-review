bec63deb06c33eb2d52fc87924dc548840a3aace
rzapriono <apriono.reza@gmail.com>
2024-05-26 17:04:26 +0700

    [GREEN] Add more tests for service

--- src/test/java/id/ac/ui/cs/advprog/koleksikota/ratingreview/service/RatingReviewServiceTest.java
+++ src/test/java/id/ac/ui/cs/advprog/koleksikota/ratingreview/service/RatingReviewServiceTest.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.koleksikota.ratingreview.service;
 
+import id.ac.ui.cs.advprog.koleksikota.ratingreview.command.EditRatingReviewCommand;
 import id.ac.ui.cs.advprog.koleksikota.ratingreview.command.RatingReviewCommand;
 import id.ac.ui.cs.advprog.koleksikota.ratingreview.model.Box;
 import id.ac.ui.cs.advprog.koleksikota.ratingreview.model.RatingReview;
@@ -16,11 +17,10 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -84,13 +84,56 @@ class RatingReviewServiceTest {
         assertTrue(allRatingReview.contains(ratingReview2));
     }
 
-//    @Test
-//    public void testGetRatingReviewByBox() {
-//        String boxId = box.getBoxId();
-//        RatingReview expectedRatingReview = new RatingReview(box, "maman resing", "gg geming", 5);
-//
-//        when(restTemplate.getForEntity("http://34.124.239.11/" + boxId, RatingReview.class)).thenReturn(ResponseEntity.ok(expectedRatingReview));
-//        RatingReview actualRatingReview = ratingReviewService.findRatingReviewByBox(boxId);
-//        assertEquals(expectedRatingReview, actualRatingReview);
-//    }
+    @Test
+    void testFindRatingReviewById(){
+        RatingReview ratingReview = new RatingReview();
+        String ratingReviewId = UUID.randomUUID().toString();
+        ratingReview.setRatingReviewId(ratingReviewId);
+
+        when(ratingReviewRepository.findById(ratingReviewId)).thenReturn(Optional.of(ratingReview));
+
+        Optional<RatingReview> result = ratingReviewService.findReviewById(ratingReviewId);
+
+        assertTrue(result.isPresent());
+        assertEquals(ratingReviewId, result.get().getRatingReviewId());
+    }
+
+    @Test
+    void testFindAllRatingReviewByUserId(){
+        RatingReview ratingReview1 = new RatingReview();
+        ratingReview1.setRatingReviewId("07f9b8b0-7257-4434-a5b9-79c9703f0760");
+        RatingReview ratingReview2 = new RatingReview();
+        ratingReview2.setRatingReviewId("07f9b8b0-7257-4434-a5b9-79c9703f0760");
+        when(ratingReviewRepository.findAllByUserId("07f9b8b0-7257-4434-a5b9-79c9703f0760")).thenReturn(Arrays.asList(ratingReview1, ratingReview2));
+
+        List<RatingReview> reviews = ratingReviewService.findAllByUserId("07f9b8b0-7257-4434-a5b9-79c9703f0760");
+        assertFalse(reviews.isEmpty());
+        assertEquals(reviews.size(), 2);
+        assertEquals(reviews.get(0).getRatingReviewId(), ratingReview1.getRatingReviewId());
+        assertEquals(reviews.get(1).getRatingReviewId(), ratingReview2.getRatingReviewId());
+    }
+
+    @Test
+    void testFindRatingReviewBySubscriptionBoxId(){
+        RatingReview ratingReview1 = new RatingReview();
+        ratingReview1.setSubscriptionBoxId("99963276-4e60-4e9a-96ce-8d5a9957209d");
+        RatingReview ratingReview2 = new RatingReview();
+        ratingReview2.setSubscriptionBoxId("99963276-4e60-4e9a-96ce-8d5a9957209d");
+
+        when(ratingReviewRepository.findAllBySubscriptionBoxId("99963276-4e60-4e9a-96ce-8d5a9957209d"))
+                .thenReturn(Arrays.asList(ratingReview1,ratingReview2));
+
+        List<RatingReview> ratingReviewsOnBox = ratingReviewService.findAllBySubscriptionBoxId("99963276-4e60-4e9a-96ce-8d5a9957209d");
+        assertFalse(ratingReviewsOnBox.isEmpty());
+        assertEquals(ratingReviewsOnBox.size(), 2);
+        assertEquals(ratingReviewsOnBox.get(0).getSubscriptionBoxId(), ratingReview1.getSubscriptionBoxId());
+        assertEquals(ratingReviewsOnBox.get(1).getSubscriptionBoxId(), ratingReview2.getSubscriptionBoxId());
+    }
+
+    @Test
+    void testFindByIdThrowsException() {
+        when(ratingReviewRepository.findById("not-exist")).thenReturn(Optional.empty());
+        Optional<RatingReview> result = ratingReviewService.findReviewById("not-exist");
+        assertFalse(result.isPresent());
+    }
 }
